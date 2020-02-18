8448dc330f1407a6e53a3fe7406131ea3b94ce07
Thomas Jakobi <thomas.jakobi@partout.info>
2020-02-18 15:16:30 +0100

    Styling issues of the install options
    - wrong position of the legend tag
    - width: 50% makes no sense

--- _build/install.options/user.input.php
+++ _build/install.options/user.input.php
@@ -48,14 +48,12 @@
  */
 
 $templates = $modx->getCollection('modTemplate');
-$output = "<p style=\"width:50%\">The getDynaDescription snippet will allow you to create description Meta tags by placing a tag in your template(s). The snippet can use the value of the dynaDescription Template variable, the description resource field, or the beginning of the resource content for the Meta tag.</p><br />
+$output = "<p>The getDynaDescription snippet will allow you to create description Meta tags by placing a tag in your template(s). The snippet can use the value of the dynaDescription Template variable, the description resource field, or the beginning of the resource content for the Meta tag.</p><br />';
+$output .= '<p>If you would like to have the dynaDescription template variable created automatically and attached to any of your templates, check the appropriate boxes below. If you plan to use the resource description field or the resource content, you won't need the TV.</p><br />';
 
-<p style=\"width:50%\">If you would like to have the dynaDescription template variable created automatically and attached to any of your templates, check the appropriate boxes below. If you plan to use the resource description field or the resource content, you won't need the TV.</p><br />";
-
-
-$output .= '<fieldset style="padding:15px;margin:0px;width:50%;"><input type="checkbox" name="createTv" value="CreateDescriptionTv"><legend>&nbsp;&nbsp;Create TV&nbsp;&nbsp;</legend>&nbsp;&nbsp;Create dynaDescription template variable<br /><br />';
+$output .= '<fieldset style="padding:15px;margin:0px"><legend>&nbsp;&nbsp;Create TV&nbsp;&nbsp;</legend>';
+$output .= '<input type="checkbox" name="createTv" value="CreateDescriptionTv">&nbsp;&nbsp;Create dynaDescription template variable<br /><br />';
 $output .= '<input type="text" name="tvDefault" value="">&nbsp;&nbsp;Default value for TV (optional - not recommended)<br /><br />';
-
 
 if (!empty ($templates) ) {
     $output .=  '<br />Select Templates to attach TV to:<br /><br />';
@@ -67,15 +65,16 @@ if (!empty ($templates) ) {
     $output .= 'No Templates<br />';
 }
 
-$output .= '<br /><p  style="width:50%">If you want a snippet tag inserted in the &lt;head&gt; section of the selected templates (be sure you have checked the templates above) select the desired option below:</p><br />';
+$output .= '<br /><p>If you want a snippet tag inserted in the &lt;head&gt; section of the selected templates (be sure you have checked the templates above) select the desired option below:</p><br />';
 
-$output .= '<fieldset style="padding:15px; margin:0px;width:50%;"><input type="radio" checked="checked" name="doTag" value="no"><legend>&nbsp;&nbsp;Tag Style&nbsp;&nbsp;</legend>&nbsp;&nbsp;No (other options will be ignored)<br />';
+$output .= '<fieldset style="padding:15px; margin:0px"><legend>&nbsp;&nbsp;Tag Style&nbsp;&nbsp;</legend>';
+$output .= '<input type="radio" name="doTag" value="no" checked="checked">&nbsp;&nbsp;No (other options will be ignored)<br />';
 $output .= '<input type="radio" name="doTag" value="useTV">&nbsp;&nbsp;[[!getDynaDescription? &amp;descriptionTv=`dynaDescription`]] (use the TV)<br />';
 $output .= '<input type="radio" name="doTag" value="useDescription">&nbsp;&nbsp;[[!getDynaDescription? &amp;useResourceDescription=`1`]] (use the resource Description field)<br />';
 $output .= '<input type="radio" name="doTag" value="noProperties">&nbsp;&nbsp;[[!getDynaDescription? &amp;descriptionTv=``]] (use resource content)</fieldset><br />';
 
-$output .= '<fieldset style="padding:15px;margin:0px;width:50%;"><legend>&nbsp;&nbsp;Options&nbsp;&nbsp;</legend><input type="checkbox" name="fullTag" value="fullTag">&nbsp;&nbsp;Create Full Tag (adds &amp;fullTag=`1` to the snippet tag to create the entire Meta tag instead of just the description part)<br /><br />';
+$output .= '<fieldset style="padding:15px;margin:0px"><legend>&nbsp;&nbsp;Options&nbsp;&nbsp;</legend>';
+$output .= '<input type="checkbox" name="fullTag" value="fullTag">&nbsp;&nbsp;Create Full Tag (adds &amp;fullTag=`1` to the snippet tag to create the entire Meta tag instead of just the description part)<br /><br />';
 $output .= '<input type="text" name="maxWords" size="4" value="25">&nbsp;&nbsp;Max words to use from content</fieldset><br /><br />';
-
 
 return $output;
